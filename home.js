3b2fbcdabafa307ff40a6b15b329efd18478d631
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-20 23:30:38 +0530

    New Commit

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
-showMessage("Changes...")
+// let name = 'Jena';
+// let message = `Hello ${name}`;
+
+// showMessage(message);
+
+// let person = {
+//     firstName: 'Ashish',
+//     lastName: 'Jena'
+// };
+
+// showMessage(person.firstName);
+
+const header = document.getElementById('message');
+header.style.color = 'blue';
+header.style.fontWeight = '100';
