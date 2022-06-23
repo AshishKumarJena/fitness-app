fd9b37c5728640fbd2711881ff47162b0dcbca06
AshishKumarJena <ashishkujena2000@gmail.com>
2022-06-23 19:14:20 +0530

    New Commit

--- home.js
+++ home.js
@@ -1,15 +1,31 @@
-// let name = 'Jena';
-// let message = `Hello ${name}`;
+let name = 'Jena';
+let message = `Hello ${name}`;
 
-// showMessage(message);
+showMessage(message);
 
-// let person = {
-//     firstName: 'Ashish',
-//     lastName: 'Jena'
-// };
+let person = {
+    firstName: 'Ashish',
+    lastName: 'Jena'
+};
 
-// showMessage(person.firstName);
+showMessage(person.firstName);
 
 const header = document.getElementById('message');
 header.style.color = 'blue';
 header.style.fontWeight = '100';
+
+const button = document.getElementById('see-review');
+button.addEventListener('click', function(){
+   const review = document.getElementById('review');
+
+   if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+        button.textContent = 'CLOSE REVIEW';
+   } else {
+        review.classList.add('d-none');
+        button.textContent = 'SEE REVIEW';
+   }
+});
+
+const values = ['a', 'b', 'c'];
+console.log(values);
